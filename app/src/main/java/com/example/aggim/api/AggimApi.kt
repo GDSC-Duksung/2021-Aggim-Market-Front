3cf4e8c3c976385381de7ca220e34f940f8c38d7
smartshk <seohkim@student.42seoul.kr>
2021-01-23 00:52:22 +0900

    add signup

--- app/src/main/java/com/example/aggim/api/AggimApi.kt
+++ app/src/main/java/com/example/aggim/api/AggimApi.kt
@@ -1,11 +1,16 @@
 package com.example.aggim.api
 
+import com.example.aggim.api.request.SignupRequest
 import com.example.aggim.api.response.*
 import retrofit2.http.*
 
 interface AggimApi {
     @GET("/api/v1/hello")
     suspend fun hello(): ApiResponse<String>
+
+    @POST("/api/v1/users")
+    suspend fun signup(@Body signupRequest: SignupRequest)
+        : ApiResponse<Void>
 
     companion object {
         val instance = ApiGenerator()
--- app/src/main/java/com/example/aggim/intro/IntroActivity.kt
+++ app/src/main/java/com/example/aggim/intro/IntroActivity.kt
@@ -9,7 +9,12 @@ import org.jetbrains.anko.setContentView
 import android.os.Bundle
 import android.util.Log
 import com.example.aggim.api.AggimApi
+import com.example.aggim.signup.SignupActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.anko.startActivity
 import java.lang.Exception
 
 class IntroActivity : Activity() {
@@ -18,13 +23,19 @@ class IntroActivity : Activity() {
         val ui = IntroActivityUI()
         ui.setContentView(this)
 
-        runBlocking {
-            try {
-                val response = AggimApi.instance.hello()
-                Log.d("IntroActivity", response.data)
-            } catch (e: Exception) {
-                Log.e("IntroActivity", "Hello API 호출 오류", e)
-            }
+        GlobalScope.launch {
+            delay(1000)
+            startActivity<SignupActivity>()
+            finish()
         }
+
+//        runBlocking {
+//            try {
+//                val response = AggimApi.instance.hello()
+//                Log.d("IntroActivity", response.data)
+//            } catch (e: Exception) {
+//                Log.e("IntroActivity", "Hello API 호출 오류", e)
+//            }
+//        }
     }
 }
