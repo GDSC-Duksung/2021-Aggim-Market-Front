75892c6ca92dba7c3f13691e15164173b9693e19
rosesua318 <rosesua@duksung.ac.kr>
2021-02-01 00:14:17 +0900

    My Page

---
+++ app/src/main/java/com/example/aggim/mypage/cart/CartActivity.kt
@@ -0,0 +1,13 @@
+package com.example.aggim.mypage.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aggim.R
+import kotlinx.android.synthetic.main.activity_my_page_main.*
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
