03255f10c2567ee652874697b531aad3f8cad619
rosesua318 <rosesua@duksung.ac.kr>
2021-03-07 23:45:26 +0900

    message

--- app/src/main/java/com/example/aggim/management/donationManage/DonationManagementDataSource.kt
+++ app/src/main/java/com/example/aggim/management/donationManage/DonationManagementDataSource.kt
@@ -1,7 +1,52 @@
 package com.example.aggim.management.donationManage
 
 import android.content.res.Resources
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.aggim.App
+import com.example.aggim.api.AggimApi
+import com.example.aggim.api.response.ApiResponse
+import com.example.aggim.api.response.DonateListItemResponse
+import com.example.aggim.api.response.DonateResponse
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.anko.toast
 
 class DonationManagementDataSource(resources: Resources) {
+    private val initialDonationList = getDonations().data
+    private val donationsLiveData = MutableLiveData(initialDonationList)
 
+    fun getDonationList() : MutableLiveData<List<DonateResponse>?> {
+        Log.i("warning message", donationsLiveData.value.toString())
+        return donationsLiveData
+    }
+
+    private fun getDonations() = runBlocking {
+        try {
+            AggimApi.instance.getDonations()
+        } catch(e: Exception) {
+            ApiResponse.error<List<DonateResponse>>(
+                "알 수 없는 오류가 발생했습니다."
+            )
+        }
+    }
+
+    private fun showErrorMessage(
+        response: ApiResponse<List<DonateListItemResponse>>
+    ) {
+        App.instance.toast(
+            response.message ?: "알 수 없는 오류가 발생했습니다."
+        )
+    }
+
+    companion object {
+        private var INSTANCE: DonationManagementDataSource? = null
+
+        fun getDataSource(resources: Resources): DonationManagementDataSource {
+            return synchronized(DonationManagementDataSource::class) {
+                val newInstance = INSTANCE ?: DonationManagementDataSource(resources)
+                INSTANCE = newInstance
+                newInstance
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/aggim/management/donationManage/DonationManagementViewModel.kt
+++ app/src/main/java/com/example/aggim/management/donationManage/DonationManagementViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.aggim.management.donationManage
 
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.aggim.donation.DonateListItemDataSource
+
+class DonationManagementViewModel(
+    val dataSource: DonationManagementDataSource
+) : ViewModel() {
+    val donationsLiveData = dataSource.getDonationList()
+}
+
+class DonatesListViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DonationManagementViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return DonationManagementViewModel(
+                dataSource = DonationManagementDataSource.getDataSource(context.resources)
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
